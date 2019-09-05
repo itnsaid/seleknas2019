0c074022333c1df7568a00119567b24024842c05
itnsaid <46662293+itnsaid@users.noreply.github.com>
2019-09-05 18:40:58 +0700

    Add files via upload

---
+++ Modul 3 - Linux Environment/db.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `competitors` (
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `name` varchar(255) NOT NULL,
+    PRIMARY_KEY(`id`)
+);
+
+INSERT INTO `competitors` (name) VALUES ('adam');
+
